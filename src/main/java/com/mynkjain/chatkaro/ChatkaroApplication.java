daf66a6ea83b268ee5a8ec4d5a1119e7c2c69c53
Mayank Jain <jainmayank1297@gmail.com>
2024-04-20 14:44:18 +0530

    Implement login with JWT

--- src/main/java/com/mynkjain/chatkaro/ChatkaroApplication.java
+++ src/main/java/com/mynkjain/chatkaro/ChatkaroApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChatkaroApplication {
+public class
+
+ChatkaroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatkaroApplication.class, args);
--- src/main/java/com/mynkjain/chatkaro/controller/AuthController.java
+++ src/main/java/com/mynkjain/chatkaro/controller/AuthController.java
@@ -3,13 +3,17 @@ package com.mynkjain.chatkaro.controller;
 import com.mynkjain.chatkaro.config.JwtProvider;
 import com.mynkjain.chatkaro.model.User;
 import com.mynkjain.chatkaro.repository.UserRepository;
+import com.mynkjain.chatkaro.request.LoginRequest;
 import com.mynkjain.chatkaro.response.AuthResponse;
+import com.mynkjain.chatkaro.service.CustomUserDetailsService;
 import com.mynkjain.chatkaro.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,6 +32,10 @@ public class AuthController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
     @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUser(@RequestBody User user) throws Exception {
         try{
@@ -53,10 +61,40 @@ public class AuthController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-
-
-
     }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signin(@RequestBody LoginRequest loginRequest){
+        try {
+            Authentication authentication = authenticate(loginRequest.getEmail(), loginRequest.getPassword());
+
+            String token = JwtProvider.generateToken(authentication);
+
+            AuthResponse res = new AuthResponse(token, "Login Successful");
+
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        }catch (Exception e) {
+
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private Authentication authenticate(String email, String password) {
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(email);
+
+
+        if(userDetails == null){
+            throw new BadCredentialsException("invalid username");
+        }
+
+        if(!passwordEncoder.matches(password,userDetails.getPassword())){
+            throw new BadCredentialsException("wrong password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+    }
+
 }
 
