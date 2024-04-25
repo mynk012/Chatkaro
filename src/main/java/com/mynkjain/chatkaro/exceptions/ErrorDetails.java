41237293425b2a14cce8bda9b22a76ee5dec34b7
Mayank Jain <jainmayank1297@gmail.com>
2024-04-25 23:36:42 +0530

    added exception handling feature

---
+++ src/main/java/com/mynkjain/chatkaro/exceptions/ErrorDetails.java
@@ -0,0 +1,18 @@
+package com.mynkjain.chatkaro.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+
+    private String message;
+    private String error;
+    private LocalDateTime timestamp;
+
+}
---
+++ src/main/java/com/mynkjain/chatkaro/exceptions/GlobalExceptions.java
@@ -0,0 +1,21 @@
+package com.mynkjain.chatkaro.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptions {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> otherExceptionHandler(Exception e, WebRequest req){
+
+        ErrorDetails error = new ErrorDetails(e.getMessage(), req.getDescription(false), LocalDateTime.now());
+
+        return new ResponseEntity<ErrorDetails>(error, HttpStatus.BAD_REQUEST);
+    }
+}
